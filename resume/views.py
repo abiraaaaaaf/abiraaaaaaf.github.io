74e3b74f933d2956ecab7d1ee6b5a15a89089e4b
Fariba Lotfi <faribalotfi@Faribas-MacBook-Air.local>
2024-08-29 15:09:09 +1000

    Updated resume information

--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,22 @@
+#from django.shortcuts import render
+
+#def home(request):
+#    return render(request, 'resume/home.html')
+
+
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'resume/home.html')
+def resume_view(request):
+    context = {
+        'name': 'Fariba',
+        'experience': [
+            'Senior Engineer at ABC Corp (2020 - Present)',
+            'Research Assistant at University of Sydney (2018 - 2020)'
+        ],
+        'education': [
+            'PhD in Power Electronics, University of Technology Sydney (2024)',
+            'Master of Engineering (Electrical), University of Sydney (2019)'
+        ]
+    }
+    return render(request, 'index.html', context)
+
